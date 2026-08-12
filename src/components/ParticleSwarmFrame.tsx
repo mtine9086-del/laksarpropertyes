ba56f18b8ade41c2add4455027f3cb1e87d8e2ee
mtine9086-del <mtine9086@gmail.com>
2026-08-12 08:52:59 +0530

    Use relative particle animation path on GitHub Pages

--- src/components/ParticleSwarmFrame.tsx
+++ src/components/ParticleSwarmFrame.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 export default function ParticleSwarmFrame() {
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? '';
-
   return (
     <div className="hero-particle-layer" aria-hidden="true">
       <iframe
         className="hero-particle-swarm"
-        src={`${basePath}/particle-swarm.html`}
+        src="./particle-swarm.html"
         title=""
         tabIndex={-1}
         loading="eager"
